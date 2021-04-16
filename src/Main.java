55b9a1a7dcb07156bb109244644cd68b49ada8cd
vhieu-nx <nguyenvhieu1208@gmail.com>
2021-04-16 14:24:32 +0700

    QueueArray

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Queue queue = new Queue();
+        queue.enqueue("nguyen");
+        queue.enqueue("van");
+        queue.enqueue("hieu");
+//        queue.dequeue();
+        queue.printQueue();
+    }
+}
